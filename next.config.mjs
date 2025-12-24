470582ce6d779272fd8952e174c4cc9109c78d88
7892244585 <punithmahi2006@gmail.com>
2025-12-24 08:55:01 +0530

    start build

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    turbo: false, // ✅ disable Turbopack (fixes Clerk build error)
-  },
 };
 
 export default nextConfig;
+
 
